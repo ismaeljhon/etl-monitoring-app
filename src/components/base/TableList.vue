f21e1e84540964a9de408287faccc5ed58c6cfd9
Ismael Jhon Jalbuna Jr <ismael.jalbuna@inriver.com>
2022-06-16 18:34:20 +0800

    add tablelist component

---
+++ src/components/base/TableList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+interface Header {
+  value: string;
+  text: string;
+}
+interface Props { 
+  items: Array<Object>;
+  headers: Header[];
+}
+defineProps<Props>();
+</script>
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th v-for="item in headers" :key="item.value">{{ item.text }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in items" :key="index">
+        <td v-for="(header, headerIndex) in headers" :key="headerIndex">
+          {{ item[header.value] }}
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
