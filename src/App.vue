740d16f327bbe86808629fa6fa837a55f3014ceb
Ismael Jhon Jalbuna Jr <ismael.jalbuna@inriver.com>
2022-06-17 14:57:33 +0800

    Update: used company component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <v-app>
     <v-main>
-      
+      <v-app-bar color="primary" density="compact">
+        <v-app-bar-title>ETL Monitoring App</v-app-bar-title>
+        <template v-slot:append>
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </template>
+      </v-app-bar>
+      <v-container>
+        <v-row>
+          <v-col cols="12" class="pt-10">
+            <Companies></Companies>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
+import Companies from './components/Companies.vue';
+
 </script>
