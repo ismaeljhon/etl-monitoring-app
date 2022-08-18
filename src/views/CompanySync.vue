276c8184102877e741b4271270e76dfe92586a13
Ismael Jhon Jalbuna Jr <ismael.jalbuna@inriver.com>
2022-08-18 14:06:46 +0800

    Update action button routing

--- src/views/CompanySync.vue
+++ src/views/CompanySync.vue
@@ -3,11 +3,12 @@ import { onMounted, ref } from "vue";
 import TableList from "../components/base/TableList.vue";
 import { WebJob } from "../interfaces/webjob.interface";
 import SyncService from "../services/SyncService";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { companySyncColumns } from '../composables/TableColumns'
 
 const webjobs = ref<WebJob[]>([]);
 const route = useRoute()
+const router = useRouter()
 const companyCode = ref<string>(route.params.company_code.toString())
 
 const refreshTable = async () => {
@@ -21,16 +22,10 @@ onMounted(async () => {
 
 <template>
   <div>
-    <div class="row">
-      <div class="col">
-        <div class="float-right q-pa-md">
-          <q-btn type="primary" @click="refreshTable">Refresh</q-btn>
-        </div>
-      </div>
-    </div>
-    <TableList title="Sync WebJobs" :rows="webjobs" :columns="companySyncColumns" :has-actions="true" row-key="name">
+    <TableList title="Sync WebJobs" :rows="webjobs" :columns="companySyncColumns" :has-actions="true" row-key="name"
+      @refresh="refreshTable">
       <template #actions="{ row }">
-        <q-btn size="sm" flat color="primary" :href="`/companies/${companyCode}/webjobs/sync/${row.name}`">
+        <q-btn size="sm" flat color="primary" @click.prevent="router.push(`sync/${row.name}`)">
           Details
         </q-btn>
       </template>
