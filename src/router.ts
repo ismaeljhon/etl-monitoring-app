6d96a3f0006ea55472ddf882841ef57c4056a6dd
Ismael Jhon Jalbuna Jr <ismael.jalbuna@inriver.com>
2022-08-18 21:04:17 +0800

    Add names on the routes for easy access

--- src/router.ts
+++ src/router.ts
@@ -3,18 +3,22 @@ import type { RouteRecordRaw } from 'vue-router';
 
 export const routes: RouteRecordRaw[] = [
   {
+    name: 'Home',
     path: '/',
     component: () => import('./views/Home.vue'),
   },
   {
+    name: 'EtlList',
     path: '/companies/:company_code/webjobs/etl',
     component: () => import('./views/companies/webjobs/EtlList.vue'),
   },
   {
+    name: 'SyncList',
     path: '/companies/:company_code/webjobs/sync',
     component: () => import('./views/companies/webjobs/SyncList.vue'),
   },
   {
+    name: 'SyncWebJobHistory',
     path: '/companies/:company_code/webjobs/sync/:webjob_name',
     component: () => import('./views/companies/webjobs/SyncWebJobHistory.vue'),
   },
