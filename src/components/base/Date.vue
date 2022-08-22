2644cc1528b2e62a5d6053ce12167bb2365cd306
Ludwig Manansala <manansalaludwig@gmail.com>
2022-08-22 10:23:26 +0800

    date component to handle datetime formats

---
+++ src/components/base/Date.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { date } from "quasar";
+
+interface Props {
+  datetime: string;
+  format?: string;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    {{ date.formatDate(datetime, format) }}
+  </div>
+</template>
