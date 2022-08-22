fdde1bc45b9bc9579e27cdd8cba0a6585cd69b85
Ismael Jhon Jalbuna Jr <ismael.jalbuna@inriver.com>
2022-08-22 15:03:04 +0800

    Add etl request form

---
+++ src/components/forms/EtlRequestForm.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>import { reactive } from 'vue';
+
+const data = reactive({
+  name: '',
+  date: ''
+})
+</script>
+<template>
+  <q-input label="Company" dense v-model="data.name" readonly autofocus />
+  <div class="q-ma-sm">
+    <q-input dense v-model="data.date" mask="date" :rules="['date']">
+      <template v-slot:append>
+        <q-icon name="event" class="cursor-pointer">
+          <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+            <q-date v-model="data.date">
+              <div class="row items-center justify-end">
+                <q-btn v-close-popup label="Close" color="primary" flat />
+              </div>
+            </q-date>
+          </q-popup-proxy>
+        </q-icon>
+      </template>
+    </q-input>
+  </div>
+
+</template>
