a7a112f77872e3614c31f614d4c035f9b3df86df
Ismael Jhon Jalbuna Jr <ismael.jalbuna@inriver.com>
2022-08-25 14:58:55 +0800

    Refactor latest run - add defaults and handle null value on the synclist

--- src/components/base/LatestRun.vue
+++ src/components/base/LatestRun.vue
@@ -1,21 +1,31 @@
 <script setup lang="ts">
-import { computed } from "@vue/reactivity";
 import { WebJobRun } from "../../interfaces/webjob.interface";
 import Date from "./Date.vue";
 
 interface Props {
-  latest_run: WebJobRun;
+  latest_run: Pick<WebJobRun, 'start_time' | 'end_time' | 'status'>;
 }
 
-withDefaults(defineProps<Props>(), {});
+withDefaults(defineProps<Props>(), {
+  latest_run: (): Pick<WebJobRun, 'start_time' | 'end_time' | 'status'> => {
+    return {
+      start_time: '',
+      end_time: '',
+      status: ''
+    }
+  }
+});
 </script>
 
 <template>
   <div>
-    <strong>Start Date:</strong> <Date :datetime="latest_run.start_time" />
+    <strong>Start Date:</strong>
+    <Date :datetime="latest_run.start_time" />
   </div>
 
-  <div><strong>End Date:</strong> <Date :datetime="latest_run.end_time" /></div>
+  <div><strong>End Date:</strong>
+    <Date :datetime="latest_run.end_time" />
+  </div>
   <strong>Status: </strong>
   <q-badge :color="latest_run.status === 'Success' ? 'green' : 'red'">
     {{ latest_run.status }}
