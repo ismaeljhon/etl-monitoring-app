d9d5fb15bfde2a3a4bdbd4ba3f521cd91f915525
Ismael Jhon Jalbuna Jr <ismael.jalbuna@inriver.com>
2022-09-02 01:27:09 +0800

    Test vite config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,21 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    server: {
-      port: 4000,
-    },
-    plugins: [
-      vue({
-        template: { transformAssetUrls },
-      }),
-      quasar({
-        sassVariables: 'src/quasar-variables.sass',
-      }),
-    ],
-    define: {
-      __APP_ENV__: env,
-    },
-  };
+export default defineConfig({
+  server: {
+    port: 4000,
+  },
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass',
+    }),
+  ],
+  define: {
+    __APP_ENV__: process.env,
+  },
 });
