3a328b5f0690a70c3ecfa1fc4b260f2985063f23
Ludwig Manansala <ludwig.manansala@inriver.com>
2022-11-24 19:23:58 +0800

    changed routing and added children under companies

--- src/router.ts
+++ src/router.ts
@@ -21,32 +21,36 @@ export const routes: RouteRecordRaw[] = [
   {
     name: "Companies",
     path: "/companies",
-    component: () => import("./views/Companies.vue"),
-    beforeEnter: [auth],
-  },
-  {
-    name: "EtlList",
-    path: "/companies/:company_code/webjobs/etl",
-    component: () => import("./views/companies/webjobs/EtlList.vue"),
-    beforeEnter: [auth],
-  },
-  {
-    name: "SyncList",
-    path: "/companies/:company_code/webjobs/sync",
-    component: () => import("./views/companies/webjobs/SyncList.vue"),
-    beforeEnter: [auth],
-  },
-  {
-    name: "SyncWebJobHistory",
-    path: "/companies/:company_code/webjobs/sync/:webjob_name",
-    component: () => import("./views/companies/webjobs/SyncWebJobHistory.vue"),
-    beforeEnter: [auth],
-  },
-  {
-    name: "EtlWebJobHistory",
-    path: "/companies/:company_code/webjobs/etl/:webjob_name",
-    component: () => import("./views/companies/webjobs/EtlWebJobHistory.vue"),
-    beforeEnter: [auth],
+    component: () => import("./layouts/Companies.vue"),
+    children: [
+      {
+        name: "CompanyList",
+        path: "list",
+        component: () => import("./views/Companies.vue"),
+      },
+      {
+        name: "EtlList",
+        path: ":company_code/webjobs/etl",
+        component: () => import("./views/companies/webjobs/EtlList.vue"),
+      },
+      {
+        name: "SyncList",
+        path: ":company_code/webjobs/sync",
+        component: () => import("./views/companies/webjobs/SyncList.vue"),
+      },
+      {
+        name: "EtlWebJobHistory",
+        path: ":company_code/webjobs/etl/:webjob_name",
+        component: () =>
+          import("./views/companies/webjobs/EtlWebJobHistory.vue"),
+      },
+      {
+        name: "SyncWebJobHistory",
+        path: ":company_code/webjobs/sync/:webjob_name",
+        component: () =>
+          import("./views/companies/webjobs/SyncWebJobHistory.vue"),
+      },
+    ],
   },
 ];
 
@@ -54,3 +58,9 @@ export const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
+
+router.beforeEach(async (to, from, next) => {
+  const isAuthenticated = await new MsalService().isAuthenticated()
+    if (to.name !== "Home" && !isAuthenticated) next({ name: "CompanyList" });
+    else next();
+})
