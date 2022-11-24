bae907da22ab2f44766b82f53b9d6032e7d4b7e3
Ludwig Manansala <ludwig.manansala@inriver.com>
2022-11-24 19:24:35 +0800

    offloaded app.vue to giveway for layouts

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <template>
   <div>
-    <q-layout view="lHh lpr lFf">
-      <q-header elevated>
-        <q-toolbar>
-          <q-btn flat :ripple="false" href="/">
-            <q-avatar square>
-              <img :src="logo" />
-            </q-avatar>
-
-            <q-toolbar-title> ETL - Sync Monitoring </q-toolbar-title>
-          </q-btn>
-        </q-toolbar>
-      </q-header>
-
-      <q-page-container>
-        <q-page class="q-pa-md">
-          <router-view></router-view>
-        </q-page>
-      </q-page-container>
-    </q-layout>
+    <router-view></router-view>
   </div>
 </template>
 
-<script setup lang="ts">
-import logo from "./assets/inriver-logo-black.png"
-</script>
+<script setup lang="ts"></script>
