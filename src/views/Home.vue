d7a1df61f7c0f0cb5b3166e85368b74bd17b486f
Ludwig Manansala <ludwig.manansala@inriver.com>
2022-11-25 13:36:04 +0800

    code cleanup and adding of new route for login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,15 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
-import MsalService from "../services/base/MsalService";
-import logo from "../assets/inriver-logo-black.png";
-import { showSimpleNotification } from "../composables/Notify";
+import { onMounted } from "vue";
 
 // declarations
 const router = useRouter();
 
-// functions
-const signIn = async () => {
-  const resp = await new MsalService().signIn();
-  if (resp) {
-    router.push("/companies/list");
-  }
-};
+onMounted(() => {
+  router.push('/auth/login')
+})
 </script>
 
 <template>
-  <div>
-    <div>
-      <div>
-        <img style="max-width: 250px;" :src="logo" alt="">
-      </div>
-    </div>
-    <div>
-      <div>
-        <q-btn @click="signIn">Test</q-btn>
-      </div>
-    </div>
-  </div>
+
 </template>
