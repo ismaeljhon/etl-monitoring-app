d280b85ca14fd94e14bf8936335be12dac9a65d2
Ismael Jalbuna <ismael.jalbuna@inriver.com>
2022-12-06 18:05:28 +0800

    Refactor etl services in preparation for save logs functionality

--- env.d.ts
+++ env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   readonly VITE_TRIGGERED_WEBJOBS_API_BASE_URL: string;
   readonly VITE_WEBJOBS_BASIC_AUTH_USERNAME: string;
   readonly VITE_WEBJOBS_BASIC_AUTH_PASSWORD: string;
+  readonly VITE_ETL_API_BASE_URL: string;
 }
 /// <reference types="vite/client" />
 interface ImportMeta {
---
+++ src/services/EvaluateEtlApiService.ts
@@ -0,0 +1,34 @@
+import ApiService from "./base/ApiService";
+import { date } from "quasar";
+import { TriggerRequestParams } from '../interfaces/evaluate-etl.interface'
+
+export default class EvaluateEtlApiService extends ApiService {
+  constructor() {
+    super({
+      baseURL: import.meta.env.VITE_ETL_API_BASE_URL,
+      headers: {
+        "x-api-key": "fceeef5e-625c-45da-9d97-eb044b7500ba",
+        "x-origin-url": "packages.detailonline.com",
+        "Content-Type": "application/json",
+      }
+    })
+  }
+
+  async triggerEtl (triggerRequestParams: TriggerRequestParams) {
+    return super
+      .request({
+        url: 'trigger/app',
+        method: "POST",
+        data: {
+          ...triggerRequestParams,
+          date: date.formatDate(triggerRequestParams.date, "YYYY-MM-DD")
+        },
+        headers: {
+          "x-api-key": "fceeef5e-625c-45da-9d97-eb044b7500ba",
+          "x-origin-url": "packages.detailonline.com",
+          "Content-Type": "application/json",
+        }
+      })
+      .then((res) => res.data);
+  }
+}
